71a7da9ff7c35c3d7319d11b613ec81976149e50
Dheeraj Reddy Kukkala <96662214+kdheerajreddy97@users.noreply.github.com>
2024-06-09 05:36:37 -0400

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,8 +33,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:dheerajcool77@gmail.com">
-          dheerajcool77@gmail.com
+        <a className="underline" href="mailto:kdheerajreddy97@gmail.com">
+          kdheerajreddy97@gmail.com
         </a>{" "}
         or through this form.
       </p>
