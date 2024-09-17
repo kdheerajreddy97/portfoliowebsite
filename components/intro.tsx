2367ec67764233a9a05bb3f1ccee76366efc498c
Dheeraj Reddy Kukkala <dk8499@g.rit.edu>
2024-09-16 21:15:25 -0500

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,13 +62,14 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Dheeraj.</span>
-        {' '}I'm a <span className="font-bold">full-stack developer</span>.
-        <br /> <span className="italic">I enjoy building sites & apps</span>.
+        <span className="font-bold">Hi, I'm Dheeraj!</span> I'm a{" "}
+        <span className="font-bold">full-stack developer</span>.
+        <br />
+        <span className="italic">I love building scalable sites & apps</span>.
       </motion.h1>
 
       <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
@@ -77,27 +78,27 @@ export default function Intro() {
       >
         <Link
           href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          className="group bg-gray-900 text-white px-6 py-3 flex items-center gap-3 rounded-full outline-none focus:ring-2 focus:ring-gray-500 hover:scale-105 hover:bg-gray-950 transition-transform"
           onClick={() => {
             setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
           }}
         >
           Contact me here{" "}
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition-transform" />
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          className="group bg-white text-black px-6 py-3 flex items-center gap-3 rounded-full outline-none border border-gray-300 hover:scale-105 hover:bg-gray-200 transition-transform"
           href="/CV.pdf"
           download
         >
           Download CV{" "}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition-transform" />
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition-transform border border-gray-300 dark:bg-white/10 dark:text-white/60"
           href="https://linkedin.com/in/kdheerajreddy"
           target="_blank"
         >
@@ -105,7 +106,7 @@ export default function Intro() {
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-110 hover:scale-110 hover:text-gray-950 active:scale-105 transition-transform border border-gray-300 dark:bg-white/10 dark:text-white/60"
           href="https://github.com/kdheerajreddy97"
           target="_blank"
         >
